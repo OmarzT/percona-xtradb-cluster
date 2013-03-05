f800bc15fe8d806ec67164a1c27f602e01a0fe5c
Francois Aichelbaum <francois@aichelbaum.com>
2013-03-05 11:46:36 +0100

    modified:   perconHa.sh

--- ha/perconHa.sh
+++ ha/perconHa.sh
@@ -11,20 +11,16 @@ VIPRW='127.0.0.3/32'
 
 ETH='eth1'
 
-novip() {
-	ifconfig $1 $2 down &> /dev/null
-}
-
 vip() {
-	if [ -e '/etc/perconHa/RW' ]; then
+	if [ -f '/etc/perconHa/RW' ]; then
 		ifconfig $ETH:1 $VIPRW
 	else
-		novip $ETH:1 $VIPRW
+		ifconfig $ETH:1 $VIPRW down
 	fi
-	if [ -e '/etc/perconHa/RO' ]; then
+	if [ -f '/etc/perconHa/RO' ]; then
 		ifconfig $ETH:2 $VIPRO
 	else
-		novip $ETH:2 $VIPRO
+		ifconfig $ETH:2 $VIPRO down
 	fi
 }
 
@@ -35,7 +31,7 @@ if [ `ps aux | grep mysqld | wc -l` -eq 0 ]; then
 fi
 
 # is in cluster ?
-if [ `mysql --force -h $MYSQLHOST -P $MYSQLPORT -u $MYSQLUSER -p$MYSQLPASS -B -N -e "SHOW STATUS WHERE Variable!name = 'wsrep_ready';" | awk '{ print $2 }'` = "ON" ]; then
+if [ `mysql --force -h $MYSQLHOST -P $MYSQLPORT -u $MYSQLUSER -p$MYSQLPASS -B -N -e "SHOW STATUS WHERE Variable_name = 'wsrep_ready';" | awk '{ print $2 }'` = "ON" ]; then
 	# is synced ?
 	if [ `mysql --force -h $MYSQLHOST -P $MYSQLPORT -u $MYSQLUSERNAME -p$MYSQLPASSW -B -N -e "SHOW STATUS WHERE Variable_name = 'wsrep_local_state_comment';" | awk '{print $2}'` = "Synced" ]; then
 		# SYNCED
@@ -43,14 +39,14 @@ if [ `mysql --force -h $MYSQLHOST -P $MYSQLPORT -u $MYSQLUSER -p$MYSQLPASS -B -N
 		exit 0
 	else
 		# NOT SYNCED
-		novip $ETH:1 $VIPRW
-		novip $ETH:2 $VIPRO
+		ifconfig $ETH:1 $VIPRW down
+		ifconfig $ETH:2 $VIPRO down
 		exit 1
 	fi
 else
 	# NOT IN CLUSTER
-	novip $ETH:1 $VIPRW
-	novip $ETH:2 $VIPRO
+	ifconfig $ETH:1 $VIPRW down
+	ifconfig $ETH:2 $VIPRO down
 	exit 1
 fi
 
