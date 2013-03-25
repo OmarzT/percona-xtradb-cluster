921f36843fc1de126be99297b0252fc40af6c8e2
Francois Aichelbaum <francois@aichelbaum.com>
2013-03-25 11:58:05 +0100

    modified:   backup/backup-mysql.sh

--- backup/backup-mysql.sh
+++ backup/backup-mysql.sh
@@ -19,9 +19,9 @@ fi
 find $DATADIR -name "*.tar.gz" -mtime +$RETENTION -print -exec rm -f {} \;
 
 # BACKUP
-echo "SHOW DATABASES;" | mysql -u $USER -h localhost -p$PASS | grep -v information_schema | while read DB; do
+echo "SHOW DATABASES;" | mysql -u $USER -h localhost -p$PASS | grep -v information_schema | grep -v Database | while read DB; do
 	mkdir -p $DATADIR/$DB
-	mysqldump -x -u $USER -p$PASS $DB > $DATADIR/$DB/$FILENAME
+	mysqldump -u $USER -p$PASS $DB > $DATADIR/$DB/$FILENAME
 	tar -czf $DATADIR/$DB/$FILENAME.tar .gz $DATADIR/$DB/$FILENAME
 	chmod 600 $DATADIR/$DB/$FILENAME.tar.gz
 done
